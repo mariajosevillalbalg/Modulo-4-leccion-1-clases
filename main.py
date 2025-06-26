030fe13476aa97fb86516382d578b7c0f5128d71
mariajosevillalbalg <mjvlalgorithmic@gmail.com>
2025-06-26 17:07:19 -0500

    class

---
+++ main.py
@@ -0,0 +1,13 @@
+#conectar módulos PIL
+
+#create clase ImageEditor
+
+    #create class constructor
+
+    #create el método "abrir y mostrar"
+
+    #create métodos para editar el original
+
+#create el objeto de la clase ImageEditor con los datos de las imágenes originales
+
+#edita la imagen y guarda el resultado
